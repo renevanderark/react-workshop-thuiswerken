e4cfbc9952b2406690e073e119882b031c61ffc0
Rene van der Ark <r.van.der.ark@gmail.com>
2018-03-19 09:56:22 +0100

    lesson4 startup

---
+++ lesson4/README.md
@@ -0,0 +1,11 @@
+# Deel 4
+
+## Welkom terug
+
+Vandaag gaan we met een backend praten. Tot nu toe heb ik je niet verteld hoe je je ```index.html``` moet openen in de browser. Misschien heb je het apache laten uitserveren, misschien heb je zoals ik tot nu toe gewoon een ```file:///``` pad gebruikt.
+
+Wat je ook hebt gekozen, om het nu simpel te maken raad ik je aan om ```je-project``` even uit te laten serveren door ```je-backend``` (in het mapje onder deze ```lesson4``` map). Zo hoef je geen CORS headers te introduceren:
+
+```sh
+
+```
--- lesson4/je-backend/server.js
+++ lesson4/je-backend/server.js
@@ -4,11 +4,26 @@ const app = express();
 
 let tasks = [];
 app.use(bodyParser.json());
-	
-app.post('/tasks', (req, res) => {
-	console.log(req.body);
 
-	res.send('hello\n');
+app.post('/tasks', (req, res) => {
+	const newTask = {
+		contactEmail: req.body.contactEmail,
+    taskName: req.body.taskName,
+    id: `${Math.random() * new Date().getTime()}`,
+    timeStamp: new Date().getTime(),
+    status: "wachtrij"
+	};
+
+	tasks.push(newTask);
+
+	res.send(JSON.stringify(newTask));
 });
+
+app.get('/tasks', (req, res) => {
+
+	res.send(JSON.stringify(tasks));
+});
+
+app.use('/', express.static(process.env.STATIC_DIR));
 
 app.listen(3000, () => console.log("app on port 3000"));
